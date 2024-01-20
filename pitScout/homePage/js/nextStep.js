3d38cb29eea1d560a8b6dd07e1b5b9cc381f8c67
armandomm09 <pabloarmando.macbeath@gmail.com>
2024-01-19 20:30:37 -0600

    Multi step

---
+++ pitScout/homePage/js/nextStep.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    let driversSection = document.getElementById("driversSection");
+    let homePageSection = document.getElementById("homePageSection");
+    let backButton = document.getElementById("back-button")
+    let nextButton = document.getElementById("next-button");
+
+    let counter = 0;
+
+    const checkStep = () => {
+
+        switch(counter){
+            
+        }
+    }
+
+
+})
