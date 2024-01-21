2e0aa87a7f360791124e99a1d914547ce15fb7e3
armandomm09 <pabloarmando.macbeath@gmail.com>
2024-01-20 22:01:49 -0600

    Multi step case break

--- pitScout/homePage/js/nextStep.js
+++ pitScout/homePage/js/nextStep.js
@@ -6,14 +6,39 @@ document.addEventListener("DOMContentLoaded", function () {
     let backButton = document.getElementById("back-button")
     let nextButton = document.getElementById("next-button");
 
-    let counter = 0;
+    var counter = 1;
 
-    const checkStep = () => {
+    console.log(counter)
 
-        switch(counter){
-            
+    
+    
+    const checkPage = pageCounter => {
+
+        switch(pageCounter){
+            case 1:
+                homePageSection.style = "display: inline;"
+                driversSection.style = "display: none;"
+                break;
+            case 2:
+                homePageSection.style = "display: none;"
+                driversSection.style = "display: inline;"
+                break;
         }
-    }
 
+    }
+    
+    backButton.addEventListener("click", () => {
+        counter -= 1;
+        checkPage(counter)
+        console.log(counter)
+
+    })
+
+    nextButton.addEventListener("click", () => {
+        counter += 1;
+        checkPage(counter)
+        console.log(counter)
+
+    })
 
 })
