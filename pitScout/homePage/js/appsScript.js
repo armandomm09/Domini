10ebc5d388c607271fb3a88de876e48d44235568
armandomm09 <pabloarmando.macbeath@gmail.com>
2024-02-08 20:20:57 -0600

    Send as JSON

--- pitScout/homePage/js/appsScript.js
+++ pitScout/homePage/js/appsScript.js
@@ -2,22 +2,19 @@
 const sheets = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/15UFKTD1KegE9WYtVfG3TSJWM6UskqyxDziANYmAP0pg/edit?usp=sharing");
 const sheet = sheets.getSheetByName("hoja1");
 
+const valuesAray = [];
+
 const doPost = (e) => {
-  
-  let data = e.parameter;
 
-  // Extract data from the form
-  let teamName = data.teamNameInput;
-  let numeroEquipo = data.numero_equipo;
-  let progInput = data.progInput;
-  let tipoDeChasis = data.tipoDeChasis;
-  let tipoDeLlanta = data.tipoDeLlanta;
-  let imagenesEquipo = data.imagenes_equipo;
+  var jsonString = e.postData.contents;
+  var jsonData = JSON.parse(jsonString);
 
-  // Add data to the sheet
-  sheet.appendRow([teamName, numeroEquipo, progInput, tipoDeChasis, tipoDeLlanta, imagenesEquipo]);
-
+  // Append data to Google Sheet
+  var objectArray = Object.values(jsonData);
+  sheet.appendRow(objectArray)
+ 
   return ContentService.createTextOutput("Se logro");
 };
+
 
 */
--- pitScout/homePage/js/sendPitScoutForm.js
+++ pitScout/homePage/js/sendPitScoutForm.js
@@ -5,17 +5,40 @@ document.addEventListener("DOMContentLoaded", function () {
     
     let formData = new FormData(form);
     
+    let formDataObject = {};
+      formData.forEach((value, key) => {
+      formDataObject[key] = value;
+      });
 
-    const response = await fetch("https://script.google.com/macros/s/AKfycbyq2EPDjltEvwNcUDn5JNa-0SSyRdcQUJMmnk450n1OteZWCaoJfAJvVP9jpC7qvS6f3A/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzrkxwXmTV6d95ZIRWN5qKXlnCJePq7isTaq6KKKBZhz6x9WucB199XMjwJ1ayCUzEAXQ/exec", {
         method: "POST",
-        body: formData
+        body: JSON.stringify(formDataObject)
       })
       .then(res => res.text())
       .then(data => data === "Se logro" ? document.getElementById("submitSuccess").classList.remove("visually-hidden") : document.getElementById("submitSuccess").classList.add("visually-hidden"))
-      .then()  
       .catch(error => console.error('Error:', error));
 
+      
 
+      console.log(formDataObject)
+
+  
     //window.location.href = "/pitScout/autonomous/auto.html"
   });
 });
+
+
+/*
+  {"teamNameInput":"The Green Machine",
+  "numero_equipo":"352",
+  "tipoDeChasis":"Swerve",
+  "motorsChassisSelect":"Neo 550",
+  "booleanTest":"true",
+  "robotWeightSelect":"X",
+  "tipoDeLlanta":"omni",
+  "robotMeasureSelect":"X",
+  "hasVisionSelect":"Select option",
+  "visionProcessSelect":"Select option",
+  "numberOfCamerasInput":"","
+  poseEstimationSelect":"Select option"}
+*/
